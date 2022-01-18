d0142b51f094d8f634bf433e13094004cc1b1c17
unknown <limarcelo90@gmail.com>
2022-01-17 21:56:01 -0300

    criacao da home-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,10 @@ import { RouterModule, Routes } from "@angular/router";
 const routes: Routes = [
   {
     path: '', pathMatch: 'full', redirectTo: 'home'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule)
   }
 ]
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRountingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,HomeComponent
+    BrowserModule,
+    AppRountingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
