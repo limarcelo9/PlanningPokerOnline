d952c5a37e5d09ab329bfbb3f43d00da16b629bd
unknown <limarcelo90@gmail.com>
2022-01-29 15:06:52 -0300

    alterando para saas e concluindo tela login

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,7 +5,7 @@ import {CdkDragDrop ,moveItemInArray, transferArrayItem} from '@angular/cdk/drag
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
@@ -14,24 +14,5 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
-
-  todo = ['Estudar Angular', 'Estudar AWS', 'Fazer Exercicios', 'Fall asleep'];
-
-  doing = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
-
-  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
-
-  drop(event: CdkDragDrop<string[]>) {
-    if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-    } else {
-      transferArrayItem(
-        event.previousContainer.data,
-        event.container.data,
-        event.previousIndex,
-        event.currentIndex,
-      );
-    }
-  }
 
 }
